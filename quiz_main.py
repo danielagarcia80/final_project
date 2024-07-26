7156a42e98e9a654882fc82cde913f16d3aa7f03
ShaneSkare <146023601+ShaneSkare@users.noreply.github.com>
2024-07-25 17:38:07 -0700

    makes it to results with apis integrated

--- quiz_main.py
+++ quiz_main.py
@@ -23,42 +23,55 @@ def index():
 def quiz(quiz_title):
     if 'username' not in session:
         return redirect(url_for('login'))
+    final_questions = {}
     quizzes_file = load_json()
-    questions = quizzes_file[quiz_title]
-    if len(questions) >= 2:
-        final_questions = random.sample(questions, 2)
-    else:
-        final_questions = questions
+    if quiz_title in quizzes_file:
+        questions = quizzes_file[quiz_title]
+        if len(questions) >= 2:
+            final_questions = random.sample(questions, 2)
+        else:
+            final_questions = questions
     count = 10 - len(final_questions)
     remaining_questions = get_data(quiz_title, count)
     finished_questions = GetImages(remaining_questions)
-    final_questions = final_questions + finished_questions
-
-    return render_template('quiz.html', quiz=final_questions)
-
+    if final_questions:
+        finished_questions = final_questions + finished_questions
+    return render_template('quiz.html', quiz=finished_questions)
 
 @app.route('/results', methods=['POST'])
 def results():
-    quiz_title = request.form.get('title')
+    quiz_category = request.form.get('category')
     quizzes_file = load_json()
-    selected_quiz = next((quiz for quiz in quizzes_file if quiz['title'] == quiz_title), None)
-    
-    total_questions = len(selected_quiz['questions'])
-    correct_answers = 0
-    
-    for question in selected_quiz['questions']:
-        selected_answer = request.form.get(f'question_{question["id"]}')
-        if selected_answer and question['options'][selected_answer]:
-            correct_answers += 1
+    question_ids = request.form.getlist('question_ids')
+    answers = []
+    if quiz_category in quizzes_file:
+        selected_category = quizzes_file[quiz_category]
+        for question in selected_category:
+            if question['id'] in question_ids:
+                answers.append(question['correctAnswer'])
+        print(question)
+    for id in question_ids:
+        if len(id) > 12:
+            answers.append(get_answers(id))
+
+    correct_total = 0
+    total_questions = len(question_ids)
+
+    for question_id, correct_answer in zip(question_ids, answers):
+        selected_answer = request.form.get(f'question_{question_id}')
+        print(selected_answer)
+        print(correct_answer)
+        if selected_answer == correct_answer:
+            correct_total += 1
     
     username = session['username']
     if username in users:
-        users[username]['scores'].append(correct_answers)
+        users[username]['scores'].append(correct_total)
     else:
         flash('User not found. Please log in again.')
         return redirect(url_for('login'))
     
-    return render_template('result.html', total_questions=total_questions, correct_answers=correct_answers)
+    return render_template('result.html', total_questions=total_questions, correct_answers=correct_total)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -122,10 +135,26 @@ def get_data(search_term, limit):
     try:
         r = requests.get(url, headers=headers, params=params)
         data = r.json()
-        return (data)
+        return data
     except:
         print('please try again')
-# gets images for questions if available
+
+# returns correct answers from API to results route.
+def get_answers(id):
+    ApiKeyAuth = 'ApiKeyAuth'
+    url = f'https://the-trivia-api.com/v2/question/{id}'
+    headers = {
+        'X-API-Key': ApiKeyAuth
+    }
+    try:
+        r = requests.get(url, headers=headers)
+        data = r.json()
+        print(data)
+        return data['correctAnswer']
+    except:
+        print('please try again')
+
+# gets images for questions if available. Only 50 requests an hour.
 def GetImages(questions):
     API_key = '90dGU9XIi9x7RkSKXUGzP1XNOkPgptIC2icAlDeLc90'
     endpoint = 'https://api.unsplash.com/search/photos'
